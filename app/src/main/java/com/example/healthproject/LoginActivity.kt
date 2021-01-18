fcb1c7d3fa1fb3f60b97f5d181790609460b0881
Thiago Circuncisao <thiagocircuncisao@gmail.com>
2021-01-18 20:42:41 -0300

    first comit

---
+++ app/src/main/java/com/example/healthproject/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.example.healthproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        signUpButton.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+    }
+
+
+    override fun onBackPressed() {
+    }
+}
