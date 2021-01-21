462f21f65dd3f0413885f76877809fb0ece4768f
Thiago Circuncisao <thiagocircuncisao@gmail.com>
2021-01-21 20:46:44 -0300

    Adionadas novas telas relacionadas diretamente a saúde

--- app/src/main/java/com/example/healthproject/HomeActivity.kt
+++ app/src/main/java/com/example/healthproject/HomeActivity.kt
@@ -1,14 +1,33 @@
 package com.example.healthproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        initListeners()
     }
 
     override fun onBackPressed() {
+    }
+
+    private fun initListeners() {
+        generalHealthBt.setOnClickListener {
+            startActivity(Intent(this, RealTimeHealthActivity::class.java))
+        }
+        emotionalHealthBt.setOnClickListener {
+            startActivity(Intent(this, EmotionalHealthActivity::class.java))
+        }
+        physicalHealthBt.setOnClickListener {
+            startActivity(Intent(this, PhysicalHealthActivity::class.java))
+        }
+        onlineAppointmentBt.setOnClickListener {
+            startActivity(Intent(this, OnlineAppointmentActivity::class.java))
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/healthproject/RealTimeHealthActivity.kt
@@ -0,0 +1,11 @@
+package com.example.healthproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RealTimeHealthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_real_time_health)
+    }
+}
